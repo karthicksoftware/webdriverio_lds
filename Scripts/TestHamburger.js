1636b93003d757b9b42ac3bf132e60737d560366
karunachalam <karunachalam@pch.com>
2018-01-15 15:43:11 +0530

    Initial commit to add nodejs modules

---
+++ Scripts/TestHamburger.js
@@ -0,0 +1,11 @@
+var webdriverio = require('webdriverio');
+var expect = require('chai').expect;
+
+describe('Home page validation', function() {
+    it('will validate Lotto title', function() {
+        browser.url('/');
+        var title = browser.getTitle();
+        console.log('Title is :'+title);
+        expect(title).to.be.equal('PCH Lotto | PowerPrize');
+    });     
+});  
