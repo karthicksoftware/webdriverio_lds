9c5cf85d27498c91379234caaceb7d2df93ecb94
karunachalam <karunachalam@pch.com>
2018-01-24 19:10:13 +0530

    adding mochawesome framework

--- Scripts/TestHamburger.js
+++ Scripts/TestHamburger.js
@@ -7,5 +7,5 @@ describe('Home page validation', function() {
         var title = browser.getTitle();
         console.log('Title is :'+title);
         expect(title).to.be.equal('PCH Lotto | PowerPrize');
-    });     
-});  
+    });
+});
